5611f31b324e6e71439d01c5ea54cc0c43a7e5f7
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-09 13:25:15 +0330

    js 4

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let light = document.querySelector('.light');
 let button = document.querySelector('#btn');
-
 function toggleLight() {
     light.classList.toggle('red');
 }
 let x = setInterval(toggleLight, 1000);
 
-button.addEventListener('click', function () {
 
+button.addEventListener('click', function () {
+ if(x){
+  
+ }else{
+    
+ }
 })
